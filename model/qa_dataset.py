8b37ff441f3f6e7aa72d0c170f3e67f9474f8431
masahiro nishiba <nishiba@masahironoMacBook-Pro.local>
2018-03-27 09:09:43 +0900

    update

--- model/qa_dataset.py
+++ model/qa_dataset.py
@@ -1,39 +1,57 @@
 # coding: utf-8
+import itertools
+from typing import Callable, Tuple
+
 import numpy as np
-from typing import List
 
 
 class QADataset(object):
-    def __init__(self, questions: List[np.ndarray], answers: List[np.ndarray]) -> None:
-        assert len(questions) == len(answers)
-        self.questions, self.q_indices = np.unique(questions, return_inverse=True, axis=0)
-        self.answers, self.a_indices = np.unique(answers, return_inverse=True, axis=0)
+    def __init__(self, questions: np.ndarray, answers: np.ndarray) -> None:
+        assert questions.shape == answers.shape
+        assert len(questions.shape) == 2
+        self._questions, self._q_indices = np.unique(questions, return_inverse=True, axis=0)
+        self._answers, self._a_indices = np.unique(answers, return_inverse=True, axis=0)
 
-    def random_sampling(self, negative_sampling_rate=1.0):
+    def random_sampling(self, negative_sampling_rate=1.0, include_positive=True) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
         # sizes
-        positive_sampling_size = len(self.q_indices)
+        positive_sampling_size = len(self._q_indices)
         negative_sampling_size = int(positive_sampling_size * negative_sampling_rate)
         sampling_size = positive_sampling_size + negative_sampling_size
 
         # choose indices
-        qi = np.random.choice(range(len(self.questions)), sampling_size)
-        ai = np.random.choice(range(len(self.answers)), sampling_size)
+        qi = np.random.choice(range(len(self._questions)), 2 * sampling_size)
+        ai = np.random.choice(range(len(self._answers)), 2 * sampling_size)
 
         # choose unique pairs
-        original_pair_indices = set(zip(self.q_indices, self.a_indices))
+        original_pair_indices = set(zip(self._q_indices, self._a_indices))
         candidate_pair_indices = set(zip(qi, ai))
-        new_pair_indices = np.array(list(candidate_pair_indices - original_pair_indices))
-        new_pair_indices = new_pair_indices[np.random.choice(range(len(new_pair_indices)), size=negative_sampling_size, replace=False)]
-        new_q_indices = new_pair_indices[:, 0]
-        new_a_indices = new_pair_indices[:, 1]
+        negative_pair_indices = np.array(list(candidate_pair_indices - original_pair_indices))
+        negative_pair_indices = negative_pair_indices[np.random.choice(range(negative_pair_indices.shape[0]), size=negative_sampling_size, replace=False)]
+        negative_q_indices = negative_pair_indices[:, 0]
+        negative_a_indices = negative_pair_indices[:, 1]
 
-        # merge
-        labels = np.array([1] * len(self.q_indices) + [0] * len(new_q_indices))
-        q = np.append(self.q_indices, new_q_indices)
-        a = np.append(self.a_indices, new_a_indices)
-        indices = np.array(range(len(q)))
+        labels = np.zeros(len(negative_q_indices))
+        return self._shuffle(*self._add_positive(negative_q_indices, negative_a_indices, labels, include_positive))
+
+    def max_sampling(self, scoring_functions: Callable[[np.ndarray, np.ndarray], float], include_positive=True) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        # calculate scores
+        scores = np.array([scoring_functions(x, y) for x, y in itertools.product(self._questions, self._answers)]).reshape([len(self._questions), -1])
+        scores[self._q_indices, self._a_indices] = 0.0  # set the scores of positive pairs to 0.
+
+        # set indices
+        negative_q_indices = np.array(range(self._questions.shape[0]))
+        negative_a_indices = np.argmax(scores, axis=1)
+        labels = np.zeros(negative_q_indices.shape[0])
+        return self._shuffle(*self._add_positive(negative_q_indices, negative_a_indices, labels, include_positive))
+
+    def _add_positive(self, q_indices, a_indices, labels, include_positive: bool) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        if include_positive:
+            labels = np.append(labels, np.ones(len(self._q_indices)))
+            q_indices = np.append(q_indices, self._q_indices)
+            a_indices = np.append(a_indices, self._a_indices)
+        return q_indices, a_indices, labels
+
+    def _shuffle(self, q_indices, a_indices, labels) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        indices = np.array(range(len(q_indices)))
         np.random.shuffle(indices)
-        return self.questions[q[indices]], self.answers[a[indices]], labels[indices]
-
-    def max_sampling(self):
-        pass
+        return self._questions[q_indices[indices]], self._answers[a_indices[indices]], labels[indices]
--- test/model/test_qa_dataset.py
+++ test/model/test_qa_dataset.py
@@ -5,23 +5,30 @@ from model.qa_dataset import QADataset
 
 
 class TestQADataset(unittest.TestCase):
-    def setUp(self):
-        questions = [np.array([1, 1]), np.array([2, 2]), np.array([3, 3]), np.array([4, 4])]
-        answers = [np.array([10, 1]), np.array([20, 2]), np.array([30, 3]), np.array([40, 4])]
-        self.dataset = QADataset(questions=questions, answers=answers)
-        self.original_pairs = list(zip(questions, answers))
-
     def test_random_sampling(self):
-        questions, answers, labels = self.dataset.random_sampling(negative_sampling_rate=1.0)
+        questions = np.array([[1, 1], [2, 2], [3, 3], [4, 4]])
+        answers = np.array([[10, 1], [20, 2], [30, 3], [40, 4]])
+        dataset = QADataset(questions=questions, answers=answers)
+        original_pairs = list(zip(questions, answers))
+
+        questions, answers, labels = dataset.random_sampling(negative_sampling_rate=1.0)
         for q, a, label in zip(questions, answers, labels):
-            includes = self._includes(q, a)
+            includes = self._includes_in_original(q, a, original_pairs)
             if label == 0:
                 self.assertFalse(includes)
             else:
                 self.assertTrue(includes)
 
-    def _includes(self, question, answer):
-        for q, a in self.original_pairs:
+    def test_max_sampling(self):
+        questions = np.array([1, 2, 3, 4]).reshape([4, -1])
+        answers = np.array([1.1, 2, 3.1, 1]).reshape([4, -1])
+        dataset = QADataset(questions=questions, answers=answers)
+        negative_questions, negative_answers, negative_labels = dataset.max_sampling(lambda x, y: abs(x - y), include_positive=False)
+        expected_pairs = {(1, 3.1), (2, 3.1), (3, 1), (4, 1.1)}
+        self.assertEqual(expected_pairs, set(zip(negative_questions.reshape(-1), negative_answers.reshape(-1))))
+
+    def _includes_in_original(self, question, answer, original_pairs):
+        for q, a in original_pairs:
             if np.array_equal(question, q) & np.array_equal(answer, a):
                 return True
         return False
